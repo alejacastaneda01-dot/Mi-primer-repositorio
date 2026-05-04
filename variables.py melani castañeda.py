9f107eab3b1d40aff86d91271dbcddb0eb2bed9b
alejacastaneda01-dot <alejacastaneda01@gmail.com>
2026-05-04 00:06:13 -0500

    primer ejercicio con python

---
+++ variables.py melani castañeda.py
@@ -0,0 +1,35 @@
+#Esto es un comentrio de una sola linea
+"""Esto es un comentario
+de varias lineas"""
+
+#Inicializando variable
+nombre="melani castañeda"
+edad=16
+estado=True
+nota=5.0
+
+#mostrar el contenido de las variables print()
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
+#que tipo de dato tiene cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+
+#vamos a utilizar la funcion input para recoger datos por medio del teclado.
+nombre=input("¿como te llamas?")
+edad=input("¿que edad tienes?")
+estado=input("¿en que estado te encuentras?")
+nota=input("¿cual es tu nota?")
+
+#para visualizar que guardamos en las variables anteriores. 
+print("hola",nombre)
+print("tu edad es:",edad)
+print("tu estado es:",estado)
+print("tu nota es:",nota)
+
+ 
